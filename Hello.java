1e5f7110a666d33b3113ad08ab5e10e07424ea36
justinei1997 <84173104+justinei1997@users.noreply.github.com>
2021-07-14 14:55:43 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Ivan");
+        System.out.println("Hello Ivan YOU MADE SOME CHANGES");
 //
 //        int myFirstNumber = (10 + 5) + (2 * 10);
 //        int mySecondNumber = 12;
